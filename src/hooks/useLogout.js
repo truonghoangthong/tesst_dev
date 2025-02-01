267d39d273f1b6f087394431dcab6bfe0a4af3e0
khoidm2004 <khoidm2004@gmail.com>
2025-02-01 15:27:32 +0200

    Create logout function

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,25 @@
+import { useAuthStore } from "../store/authStore";
+import { useSignOut } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/firebase";
+
+const useLogout = () => {
+  const logout = useAuthStore((state) => state.logout);
+  const [signOut, loading, error] = useSignOut(auth);
+
+  const handleLogout = async () => {
+    try {
+      await signOut();
+      logout();
+      return {
+        Title: "Success",
+        Message: "Logout successfully",
+        Status: "success",
+      };
+    } catch (error) {
+      return { Title: "Error", Message: error.message, Status: "error" };
+    }
+  };
+  return { handleLogout, loading, error };
+};
+
+export default useLogout;
