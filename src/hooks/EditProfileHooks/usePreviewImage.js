9f0c119149d5207d0ffe81fcd6dbab4e0a1a4c6d
khoidm2004 <khoidm2004@gmail.com>
2025-02-12 10:34:13 +0200

    Delete redundant documentation

--- src/hooks/EditProfileHooks/usePreviewImage.js
+++ src/hooks/EditProfileHooks/usePreviewImage.js
@@ -14,26 +14,6 @@ import { useState } from "react";
  * @property {string} error.Message - Error message describing the issue.
  * @property {string} error.Status - Status indicator (e.g., "error").
  * @property {Function} handleImageChange - Function to handle file selection.
- *
- * @example
- * import React from "react";
- * import usePreviewImage from "./usePreviewImage";
- *
- * const ImageUploader = () => {
- *  const { selectedFile, error, handleImageChange, uploadToGoogleCloud } = usePreviewImage();
- *
- *  return (
- *    <div>
- *      <input type="file" accept="image/*" onChange={handleImageChange} />
- *      {error && <p style={{ color: "red" }}>{error.Message}</p>}
- *      {selectedFile && <img src={selectedFile} alt="Preview" style={{ width: "200px" }} />}
- *      <button onClick={uploadToGoogleCloud}>Upload to Google Cloud</button>
- *    </div>
- *  );
- * };
- *
- * export default ImageUploader;
- *
  */
 
 const usePreviewImage = () => {
