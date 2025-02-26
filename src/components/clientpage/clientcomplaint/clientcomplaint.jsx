156f2cf225dba5624383835a6ecae7b74f739d9c
nhi_ng_nguyen <lamnhi.ngngoc@gmail.com>
2025-02-27 00:55:56 +0200

    Backend integrate in client complaint page

--- src/components/clientpage/clientcomplaint/clientcomplaint.jsx
+++ src/components/clientpage/clientcomplaint/clientcomplaint.jsx
@@ -1,43 +1,138 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './clientcomplaint.css';
+import useAuthStore from "../../../../../Backend/src/store/authStore";
+import useFeedbackStore from '../../../../../Backend/src/store/feedbackStore';
 
 const ClientComplaint = () => {
+  const user = useAuthStore((state) => state.user);
+  const { addFeedback } = useFeedbackStore(); 
+
+  const [formData, setFormData] = useState({
+    uid: user.uid,
+    fullName: user.fullName,
+    complaintTitle: '',
+    complaintContent: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newFeedback = {
+      client: {
+        fullName: formData.fullName,
+        uid: formData.uid,
+      },
+      complaint: {
+        complaintTitle: formData.complaintTitle,
+        complaintContent: formData.complaintContent,
+      },
+      createdAt: new Date(),
+    };
+
+    const result = await addFeedback(newFeedback);
+
+    if (result.Status === 'success') {
+      alert(result.Message);
+      setFormData({
+        uid: user.uid,
+        fullName: user.fullName,
+        complaintTitle: '',
+        complaintContent: ''
+      });
+    } else {
+      alert(`Error: ${result.Message}`);
+    }
+  };
+
   return (
     <div className="complaint-form">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="complaint-form-row">
           <div className="complaint-form-group">
             <label htmlFor="fullName">Full Name</label>
-            <input type="text" id="fullName" name="fullName" placeholder="Full Name" required />
+            <input
+              type="text"
+              id="fullName"
+              name="fullName"
+              value={user.fullName}
+              placeholder={user.fullName}
+              required
+              readOnly
+            />
           </div>
           <div className="complaint-form-group">
-            <label htmlFor="email">Email Address</label>
-            <input type="email" id="email" name="email" placeholder="Email Address" required />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={user.email}
+              placeholder={user.email}
+              required
+              readOnly
+            />
           </div>
         </div>
 
         <div className="complaint-form-row">
           <div className="complaint-form-group">
             <label htmlFor="room">Room</label>
-            <input type="text" id="room" name="room" placeholder="Room" required />
+            <input
+              type="text"
+              id="room"
+              name="room"
+              value={user.room}
+              placeholder={user.room}
+              required
+              readOnly
+            />
           </div>
           <div className="complaint-form-group">
             <label htmlFor="phone">Phone Number</label>
-            <input type="tel" id="phone" name="phone" placeholder="Phone Number" required />
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              value={user.phoneNum}
+              placeholder={user.phoneNum}
+              required
+              readOnly
+            />
           </div>
         </div>
 
-        <div className="complaint-form-group reason">
-          <label htmlFor="reason">Reason for Complaint</label>
-          <input type="text" id="reason" name="reason" placeholder="Reason" required />
+        <div className="complaint-form-group">
+          <label htmlFor="complaintTitle">Reason for Complaint</label>
+          <input
+            type="text"
+            id="complaintTitle"
+            name="complaintTitle"
+            value={formData.complaintTitle}
+            onChange={handleChange}
+            placeholder="Reason"
+            required
+          />
         </div>
 
         <div className="complaint-form-group details">
-          <label htmlFor="details">Please provide any details</label>
-          <textarea id="details" name="details" rows="5" placeholder="Details"></textarea>
+          <label htmlFor="complaintContent">Please provide any details</label>
+          <textarea
+            id="complaintContent"
+            name="complaintContent"
+            value={formData.complaintContent}
+            onChange={handleChange}
+            rows="5"
+            placeholder="Details"
+          />
         </div>
 
-        <button type="submit" className="complaint-submit-button">Submit</button>
+        <button type="submit" className="complaint-submit-button">
+          Submit
+        </button>
       </form>
     </div>
   );
