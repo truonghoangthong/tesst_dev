17931e343d5186a987077616a9c024e63c294f9d
khoidm2004 <khoidm2004@gmail.com>
2025-03-11 00:12:57 +0200

    Create booking feature

---
+++ src/store/roomBookingStore.js
@@ -0,0 +1,148 @@
+import { create } from "zustand";
+import { firestore } from "../firebase/firebase";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
+
+const useRoomBookingStore = create((set) => ({
+  // Map data from array
+  roomBookings: [],
+
+  /* Data example 
+        var roomBooking = {
+            bookingId: string
+
+            bookingPeriod {
+                endAt: timestamp
+                startFrom: timestamp
+            }
+
+            client {
+                fullName: string
+                uid: string
+            }
+
+            room: string
+        }
+    */
+
+  addRoomBooking: async (newRoomBooking) => {
+    try {
+      const roomBookingRef = collection(firestore, "roomBooking");
+      const docRef = doc(roomBookingRef);
+
+      const roomBookingData = {
+        ...newRoomBooking,
+        bookingId: docRef.id,
+      };
+
+      await setDoc(docRef, roomBookingData);
+
+      set((state) => ({
+        roomBookings: [...state.roomBookings, roomBookingData],
+      }));
+
+      return {
+        Title: "Success",
+        Message: "Room booking added successfully",
+        Status: "success",
+      };
+    } catch (error) {
+      return {
+        Title: "Error",
+        Message: error.message,
+        Status: "error",
+      };
+    }
+  },
+
+  fetchRoomBookings: async () => {
+    try {
+      const roomBookingCollection = collection(firestore, "roomBooking");
+      const roomBookingSnapshot = await getDocs(roomBookingCollection);
+      const roomBookingList = roomBookingSnapshot.docs.map((doc) => ({
+        bookingId: doc.id,
+        ...doc.data(),
+      }));
+
+      set({ roomBookings: roomBookingList });
+
+      return {
+        Title: "Success",
+        Message: "Room booking fetched successfully",
+        Status: "success",
+      };
+    } catch (error) {
+      return {
+        Title: "Error",
+        Message: error.message,
+        Status: "error",
+      };
+    }
+  },
+
+  deleteRoomBooking: async (roomBookingId, userId) => {
+    try {
+      const roomBookingRef = collection(firestore, "roomBooking");
+      const roomQuery = query(
+        roomBookingRef,
+        where("bookingId", "==", roomBookingId)
+      );
+      const roomBookingSnapshot = await getDocs(roomQuery);
+
+      if (roomBookingSnapshot.empty) {
+        return {
+          Title: "Error",
+          Message: "Booking not found",
+          Status: "error",
+        };
+      }
+
+      const bookingData = roomBookingSnapshot.docs[0].data();
+
+      // Validate ownership
+      if (bookingData.client.uid !== userId) {
+        return {
+          Title: " Error",
+          Message: "You can only modify your own booking",
+          Status: "error",
+        };
+      }
+
+      // Validate time (past bookings cannot be modified)
+      if (bookingData.bookingPeriod.startFrom.toMillis() < Date.now()) {
+        return {
+          Title: "Error",
+          Message: "Past booking cannot be modified",
+          Status: "error",
+        };
+      }
+
+      await deleteDoc(roomBookingRef);
+
+      set((state) => ({
+        roomBookings: [
+          state.roomBookings.filter(
+            (roomBooking) => roomBooking.bookingId !== roomBookingId
+          ),
+        ],
+      }));
+
+      return {
+        Title: "Success",
+        Message: "Delete Room booking successfully",
+        Status: "success",
+      };
+    } catch (error) {
+      return { Title: "Error", Message: error.message, Status: "error" };
+    }
+  },
+}));
+
+export default useRoomBookingStore;
---
+++ src/store/saunaBookingStore.js
@@ -0,0 +1,146 @@
+import { create } from "zustand";
+import { firestore } from "../firebase/firebase";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
+
+const useSaunaBookingStore = create((set) => ({
+  // Map data from array
+  saunaBookings: [],
+
+  /* Data example 
+        var saunaBooking = {
+            saunaBookingId: string
+
+            bookingPeriod {
+                endAt: timestamp
+                startFrom: timestamp
+            }
+
+            client {
+                fullName: string
+                uid: string
+            }
+        }
+    */
+
+  addSaunaBooking: async (newSaunaBooking) => {
+    try {
+      const saunaBookingRef = collection(firestore, "saunaBooking");
+      const docRef = doc(saunaBookingRef);
+
+      const saunaBookingData = {
+        ...newSaunaBooking,
+        saunaBookingId: docRef.id,
+      };
+
+      await setDoc(docRef, saunaBookingData);
+
+      set((state) => ({
+        saunaBookings: [...state.saunaBookings, saunaBookingData],
+      }));
+
+      return {
+        Title: "Success",
+        Message: "Sauna booking added successfully",
+        Status: "success",
+      };
+    } catch (error) {
+      return {
+        Title: "Error",
+        Message: error.message,
+        Status: "error",
+      };
+    }
+  },
+
+  fetchSaunaBookings: async () => {
+    try {
+      const saunaBookingCollection = collection(firestore, "saunaBooking");
+      const saunaBookingSnapshot = await getDocs(saunaBookingCollection);
+      const saunaBookingList = saunaBookingSnapshot.docs.map((doc) => ({
+        saunaBookingId: doc.id,
+        ...doc.data(),
+      }));
+
+      set({ saunaBookings: saunaBookingList });
+
+      return {
+        Title: "Success",
+        Message: "Sauna booking fetched successfully",
+        Status: "success",
+      };
+    } catch (error) {
+      return {
+        Title: "Error",
+        Message: error.message,
+        Status: "error",
+      };
+    }
+  },
+
+  deleteSaunaBooking: async (saunaBookingId, userId) => {
+    try {
+      const saunaBookingRef = collection(firestore, "saunaBooking");
+      const saunaQuery = query(
+        saunaBookingRef,
+        where("saunaBookingId", "==", saunaBookingId)
+      );
+      const saunaBookingSnapshot = await getDocs(saunaQuery);
+
+      if (saunaBookingSnapshot.empty) {
+        return {
+          Title: "Error",
+          Message: "Booking not found",
+          Status: "error",
+        };
+      }
+
+      const bookingData = saunaBookingSnapshot.docs[0].data();
+
+      // Validate ownership
+      if (bookingData.client.uid !== userId) {
+        return {
+          Title: " Error",
+          Message: "You can only modify your own booking",
+          Status: "error",
+        };
+      }
+
+      // Validate time (past bookings cannot be modified)
+      if (bookingData.bookingPeriod.startFrom.toMillis() < Date.now()) {
+        return {
+          Title: "Error",
+          Message: "Past booking cannot be modified",
+          Status: "error",
+        };
+      }
+
+      await deleteDoc(saunaBookingRef);
+
+      set((state) => ({
+        saunaBookings: [
+          state.saunaBookings.filter(
+            (saunaBooking) => saunaBooking.saunaBookingId !== saunaBookingId
+          ),
+        ],
+      }));
+
+      return {
+        Title: "Success",
+        Message: "Delete sauna booking successfully",
+        Status: "success",
+      };
+    } catch (error) {
+      return { Title: "Error", Message: error.message, Status: "error" };
+    }
+  },
+}));
+
+export default useSaunaBookingStore;
